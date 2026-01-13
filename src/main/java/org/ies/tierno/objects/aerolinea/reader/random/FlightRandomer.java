ce3223155336995c98ad3a8153127be59e88f929
CarlosCRY <trueccr@gmail.com>
2026-01-13 13:48:21 +0100

    Implementando interfaz (02)

--- src/main/java/org/ies/tierno/objects/aerolinea/reader/random/FlightRandomer.java
+++ src/main/java/org/ies/tierno/objects/aerolinea/reader/random/FlightRandomer.java
@@ -1,21 +1,17 @@
 package org.ies.tierno.objects.aerolinea.reader.random;
 
 import org.ies.tierno.objects.aerolinea.model.Flight;
+import org.ies.tierno.objects.aerolinea.model.Passenger;
 import org.ies.tierno.objects.aerolinea.reader.FlightReader;
+import org.ies.tierno.objects.aerolinea.reader.PassengerReader;
 
 import java.util.Random;
 
 public class FlightRandomer implements FlightReader {
-    private final static String[] NAMES = {
-            // 21 [20]
-            "Abigail", "Benito", "Celia", "David", "Emilia", "Fernando", "Gloria", "Hugo", "Isabel", "Juan", "Laura",
-            "María", "Nicolás", "Olivia", "Pablo", "Raquel", "Santiago", "Tatiana", "Víctor", "Yolanda", "Zacarías"
-    };
-
-    private final static String[] SURNAMES = {
-            // 14 [13]
-            "Aguilar", "Barrantes", "Cortázar", "Garza", "Huertas", "Iñiguez", "Morales",
-            "Osorio", "Ponce", "Quesada", "Rivas", "Serrano", "Urriaga", "Vázquez"
+    private final static String[] CITIES = {
+            // 16 [15]
+            "Atlanta", "Beijing", "Casablanca", "Düsseldorf", "Estambul", "Hanoi", "Incheon", "Johannesburgo",
+            "Londres", "Madrid", "Osaka", "París", "Roma", "Santiago", "Toronto", "Zúrich"
     };
 
     private final Random random;
@@ -26,11 +22,18 @@ public class FlightRandomer implements FlightReader {
 
     @Override
     public Flight read () {
+        String origin = CITIES[random.nextInt(CITIES.length)];
+        String destination;
+        do {
+            destination = CITIES[random.nextInt(CITIES.length)];
+        } while (destination.equals(origin));
+
         return new Flight (
                 random.nextInt(1000, 9999),
-                NAMES[random.nextInt(NAMES.length)],
-                SURNAMES[random.nextInt(SURNAMES.length)],
-                random.nextInt(1, 200)
+                origin,
+                destination,
+                "T" + random.nextInt(1, 4),
+                passengers
                 );
     }
 
